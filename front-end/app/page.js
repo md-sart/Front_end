1c74701d2a18b7e5e3670427397aa51d8aa452d8
Maria <110576299+md-sart@users.noreply.github.com>
2025-02-14 00:41:36 +0000

    .

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
 
+function Texto1() {
+  return <p>Um outro parágrafo de texto!</p>;
+}
 export default function Home() {
   const aula = 2;
   return (
   <div>
   <h1>Olá Turma!!!!!!!!</h1>
   <p>Essa é a nossa {aula}ª aula de React!</p>
+  <Texto />
+  <Texto />
   </div>
-  )
+  );
 }
 
