8b4f043629aacbcf5b6ac5ee07ace15300fe0ef4
Maria <110576299+md-sart@users.noreply.github.com>
2025-02-14 00:32:34 +0000

    next.js

---
+++ front-end/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  const aula = 2;
+  return (
+  <div>
+  <h1>Olá Turma!!!!!!!!</h1>
+  <p>Essa é a nossa {aula}ª aula de React!</p>
+  </div>
+  )
+}
+
