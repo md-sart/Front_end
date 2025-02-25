50556232a92d5256996a100a4d21234d6969d785
Maria <110576299+md-sart@users.noreply.github.com>
2025-02-25 00:13:02 +0000

    .

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,17 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 function Texto1() {
   return <p>Um outro parágrafo de texto!</p>;
 }
+
 export default function Home() {
+  const [hide, setHide] = useState(false);
   const aula = 2;
   return (
-  <div>
-  <h1>Olá Turma!!!!!!!!</h1>
-  <p>Essa é a nossa {aula}ª aula de React!</p>
-  <Texto />
-  <Texto />
-  </div>
+    <div>
+      <h1>Olá Turma!!!!</h1>
+      <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+      <hr />
+      {!hide && (
+        <>
+          <Texto1 />
+          <Texto1 />
+        </>
+      )}
+    </div>
   );
 }
-
