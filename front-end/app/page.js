f3234e818b4746b310ee1807e3701a5535167e62
Maria <110576299+md-sart@users.noreply.github.com>
2025-03-14 01:59:48 +0000

    .

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,19 +1,27 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link"; // Importação necessária
 
 function Texto1() {
   return <p>Um outro parágrafo de texto!</p>;
 }
 
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
 export default function Home() {
   const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(0);
   const aula = 2;
   return (
     <div>
       <h1>Olá Turma!!!!</h1>
       <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+      </button>
       <hr />
       <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
       <hr />
@@ -23,10 +31,6 @@ export default function Home() {
           <Texto1 />
         </>
       )}
-      <hr />
-      <Link href="/ex01">
-        <button>Ir para Ex01</button>
-      </Link>
     </div>
   );
 }
