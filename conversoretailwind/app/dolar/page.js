70404e33fac0e6fbd7d0172d2893bdfbde617f9b
Maria <110576299+md-sart@users.noreply.github.com>
2025-04-25 02:08:28 +0000

    .

--- conversoretailwind/app/dolar/page.js
+++ conversoretailwind/app/dolar/page.js
@@ -33,6 +33,8 @@ export default function Dolar() {
     <main className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] text-center flex flex-col items-center gap-8">
       <h1 className="text-3xl sm:text-4xl font-bold">Conversor de Dólar</h1>
       <p className="text-lg sm:text-xl max-w-xl">
+        Converta automaticamente entre real e dólar
+        <br></br>
         Cotação fixa do dólar em 14/04/2025: R$ 5,68
       </p>
 
